ed91e3828a075d4d1509c91e927956ade4cccb4c
Tahir <tahirlanre@hotmail.com>
2017-06-29 15:34:09 +0100

    Initial commit

---
+++ houseaffairs_account_usability/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Account usability for House Affairs Nigeria Limited',
+    'summary': 'User friendly features.',
+    'description': """
+        -User interface improvements to Journal Entries""",
+    'category': 'Account',
+    'author': 'Tahir Aduragba',
+    'depends': ['account'],
+    'data': [
+        'views/account_views.xml',
+    ],
+}
