447972f785fe965ce9c2eacf6fc5cfd7ff47246d
Tahir <tahirlanre@hotmail.com>
2017-07-29 14:45:39 +0100

    Initital commit

---
+++ asset_usability/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Asset Usability',
+    'description': '',
+    'author': 'Tahir Aduragba',
+    'depends': ['account_asset','account'],
+    'data': [
+        'views/account_asset.xml',
+    ],
+}
+
+# TODO  create fixed asset register
