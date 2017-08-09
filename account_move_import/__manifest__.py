df433e495bfc69a80126dab4d8712ab3722e41b1
Tahir <tahirlanre@hotmail.com>
2017-08-09 12:10:48 +0100

    Initial commit

---
+++ account_move_import/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Account Move Import',
+    'description': '',
+    'version': '10.0.1.0',
+    'author': 'Tahir Aduragba',
+    'category': 'Accounting',
+    'depends': ['account'],
+    'data': ['views/account_move.xml'],
+    'installable': True,
+}
