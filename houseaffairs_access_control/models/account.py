29ac1545daf6515b48c145197cf2bd0d7a51ce56
Tahir <tahirlanre@hotmail.com>
2017-08-28 09:10:22 +0100

    Update

--- houseaffairs_access_control/models/account.py
+++ houseaffairs_access_control/models/account.py
@@ -9,8 +9,8 @@ class Account(models.Model):
     can_send = fields.Boolean(string="Can send in Internal Transfers", default=False)
     can_receieve = fields.Boolean(string="Can receieve in Internal Transfers", default=False)
     
-class AccountPayment(models.Model):
+"""class AccountPayment(models.Model):
     _inherit = "account.payment"
     
     destination_journal_id = fields.Many2one('account.journal', string='Transfer To', domain=['&',('type', 'in', ('bank', 'cash')),('can_receieve','=','True')])
-    journal_id = fields.Many2one('account.journal', string='Payment Journal', required=True, domain=['&',('type', 'in', ('bank', 'cash')),('can_send','=','True')])
+    journal_id = fields.Many2one('account.journal', string='Payment Journal', required=True, domain=['&',('type', 'in', ('bank', 'cash')),('can_send','=','True')])"""
