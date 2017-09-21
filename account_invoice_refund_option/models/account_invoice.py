a0343599b0355276f4613dff52fae7118f72274a
Tahir <tahirlanre@hotmail.com>
2017-09-21 10:06:22 +0100

    If invoice is a refund, create stock move after validating invoice

--- account_invoice_refund_option/models/account_invoice.py
+++ account_invoice_refund_option/models/account_invoice.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from openerp import api, fields, models, _
 
 
 class AccountInvoice(models.Model):
@@ -31,3 +31,46 @@ class AccountInvoice(models.Model):
             if one.is_refund:
                 args = reversed(args)
             one.type = one.type.replace(*args)
+    
+    def invoice_validate(self):
+        super(AccountInvoice, self).invoice_validate()
+        for invoice in self:
+            if invoice.type in ('out_refund') and invoice.state == 'open':
+                for line in invoice.invoice_line_ids:
+                    line._generate_moves()
+        return True
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+    
+    @api.multi
+    def _get_move_values(self, qty, location_id, location_dest_id):
+        self.ensure_one()
+        return {
+            'name': _('CUSTOMER RTN: ') + (self.product_id.name or ''),
+            'product_id': self.product_id.id,
+            'product_uom': self.uom_id.id,
+            'product_uom_qty': qty,
+            'date': self.invoice_id.date_invoice,
+            'company_id': self.invoice_id.company_id.id,
+            'state': 'confirmed',
+            #'restrict_lot_id': self.prod_lot_id.id,
+            'restrict_partner_id': self.invoice_id.partner_id.id,
+            'location_id': location_id,
+            'location_dest_id': location_dest_id,
+        }
+        
+    def _generate_moves(self):
+        moves = self.env['stock.move']
+        #Quant = self.env['stock.quant']
+        location = self.env.ref('stock.stock_location_stock')
+        source_location = self.env.ref('stock.stock_location_customers')
+        for line in self:
+            if line.quantity == 0:
+                continue
+            if line.quantity > 0:
+                vals = self._get_move_values(abs(line.quantity),source_location.id,location.id)
+            move = moves.create(vals)
+            move.action_done()  #confirm move
+        return moves
+    
