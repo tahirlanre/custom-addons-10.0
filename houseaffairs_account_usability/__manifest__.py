09ed99e84818a5a17617561c2c447d5a1556ee2c
Tahir <tahirlanre@hotmail.com>
2017-09-30 07:29:31 +0100

    Auto post journal entries

--- houseaffairs_account_usability/__manifest__.py
+++ houseaffairs_account_usability/__manifest__.py
@@ -2,7 +2,8 @@
     'name': 'Accounts user interface customisation for House Affairs Nigeria Limited',
     'summary': 'Custom user interface features.',
     'description': """
-        -Hides fields that are not needed in journal entry lines""",
+        -Hides fields that are not needed in journal entry lines
+        -Automatically post journal entries""",
     'category': 'Account',
     'author': 'Tahir Aduragba',
     'depends': ['account'],
