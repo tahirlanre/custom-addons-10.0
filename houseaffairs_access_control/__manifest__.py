a28fffd29630e3a23c7556ae4336fd3d1a48e63e
Tahir <tahirlanre@hotmail.com>
2017-10-02 10:56:40 +0100

    Hide cancel invoice and refund buttons

--- houseaffairs_access_control/__manifest__.py
+++ houseaffairs_access_control/__manifest__.py
@@ -3,12 +3,12 @@
     'name': 'House Affairs Custom Access Controls',
     'description': 'Access controls and security features',
     'author': 'Tahir Aduragba',
-    'depends': ['base','sale','account'],
+    'depends': ['base','sale','account','account_cancel'],
     'data': [
         'security/houseaffairs_security.xml',
         'security/ir.model.access.csv',
         'views/salesman_views.xml',
-        'views/account_views.xml',
+        'views/account_invoice.xml',
         'views/account_move.xml',
     ],
 }
