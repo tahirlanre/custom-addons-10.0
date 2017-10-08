60e25981a8684fab3f38910fc2c03aa8bea3a39e
Tahir <tahirlanre@hotmail.com>
2017-10-08 17:27:44 +0100

    Initial commit

---
+++ partner_sales_rep/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Partner Sales Representative',
+    'description': 'Assign sales rep to partners',
+    'author': 'Tahir Aduragba',
+    'category': 'Sales',
+    'depends': ['base','sale'],
+    'data': [
+        'views/res_partner.xml',
+        'views/sales_rep.xml',
+    ],
+}
+
+# TODO  Report to show sales rep sales analysis
---
+++ partner_sales_rep/models/sales_rep.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, fields, _
+
+class SalesRep(models.Model):
+    _name = "sales.rep"    
+    
+    
+    name = fields.Char('Sales Representative', required=True, translate=True)
+    phone = fields.Char(string="Phone #")
+    active = fields.Boolean(default=True, help="If the active field is set to false, it will allow you to hide the sales rep without removing it.")
+    company_id = fields.Many2one('res.company', string='Company',
+                                 default=lambda self: self.env['res.company']._company_default_get('crm.team'))
+    user_id = fields.Many2one('res.users', string='Team Leader')
+    
