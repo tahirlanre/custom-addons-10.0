b593e1fbfa7fbbc14a5927260d1c6cb3b2f4f099
Tahir <tahirlanre@hotmail.com>
2017-12-02 12:31:06 +0100

    Disable automatic posting

--- houseaffairs_account_usability/models/account_move.py
+++ houseaffairs_account_usability/models/account_move.py
@@ -5,8 +5,8 @@ from odoo import api, fields, models, _
 class AccountMove(models.Model):
     _inherit = "account.move"
     
-    @api.model
+    """@api.model
     def create(self, vals):
         move = super(AccountMove, self).create(vals)
         move.post()
-        return move
+        return move"""
