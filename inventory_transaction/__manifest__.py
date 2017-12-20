b86bac587637d5e7f5383e5992f40d99a95e36f4
Tahir <tahirlanre@hotmail.com>
2017-12-20 17:01:57 +0100

    Initial commit

---
+++ inventory_transaction/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2017 SITAYS (tahirlanre@hotmail.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Inventory Transaction',
+    'version': '10.0.1.0.0',
+    'category': 'Warehouse',
+    'license': 'AGPL-3',
+    'summary': '',
+    'author': 'Tahir Aduragba',
+    'depends': ['stock_account','partner_sales_rep','account_invoice_margin'],
+    'data': [
+       'report.xml',
+       'report/templates/inventory_transaction.xml',
+       'wizard/inventory_transaction_wizard.xml'
+    ],
+    'installable': True,
+}
+
---
+++ inventory_transaction/wizard/inventory_transaction_wizard.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# © 2017 SITAYS (tahirlanre@hotmail.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models, fields
+
+class inventory_transaction_wizard(models.TransientModel):
+    _name = "inventory.transaction.wizard"
+    
+    start_date = fields.Date("Start date", required="True")
+    end_date = fields.Date("End date", required="True")
+    group_by = fields.Selection([('product','Product'), ('customer','Customer'),('sales_rep','Sales Representative')], string='Group by', required=True, default='product')
+    sales_rep_ids = fields.Many2many(comodel_name='sales.rep', string="Filter Sales Representatives")
+    product_ids = fields.Many2many(comodel_name='product.product', string="Filter Products")
+    partner_ids = fields.Many2many(comodel_name='res.partner', string="Filter Customers")
+    options = fields.Selection([('summary','Summary'),('detail','Detail')], string='Report Option', required=True, default='summary')
+
+    
+    @api.multi
+    def button_export_pdf(self):
+        self.ensure_one()
+        return self._export()
+        
+    def _prepare_report_transaction(self):
+        
+        return {
+                    'start_date':self.start_date,
+                    'end_date': self.end_date,
+                    'group_by_product': self.group_by == 'product',
+                    'group_by_sales_rep': self.group_by == 'sales_rep',
+                    'group_by_partner': self.group_by == 'customer',
+                    'filter_product_ids': [(6, 0, self.product_ids.ids)],
+                    'filter_partner_ids': [(6, 0, self.partner_ids.ids)],
+                    'filter_sales_rep_ids': [(6, 0, self.sales_rep_ids.ids)],
+                }
+    
+    @api.multi
+    def button_export_xlsx(self):
+        self.ensure_one()
+        return self._export(xlsx_report=True)
+        
+    def _export(self, xlsx_report=False):
+        model = self.env['report_inventory_transaction_qweb']
+        report = model.create(self._prepare_report_transaction())
+        return report.print_report(xlsx_report)
