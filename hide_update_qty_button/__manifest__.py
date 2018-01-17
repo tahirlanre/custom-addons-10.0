2363f2a686768297287be622aca3ad752d14322f
Tahir <tahirlanre@hotmail.com>
2018-01-17 13:06:51 +0100

    Initial commit

---
+++ hide_update_qty_button/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hide update button',
+    'description': '',
+    'author': 'Tahir Aduragba',
+    'depends': ['base'],
+    'data': [
+        'views/product_view.xml',
+    ],
+}
+
