096db8390cfac2b083b85af17ccfc9b2c7a8f0c0
Tahir <tahirlanre@hotmail.com>
2018-01-30 05:32:48 +0100

    Fix views

--- sale_usability/models/res_partner.py
+++ sale_usability/models/res_partner.py
@@ -9,27 +9,4 @@ import odoo.addons.decimal_precision as dp
 class Partner(models.Model):
     _inherit = 'res.partner'
     
-    @api.multi
-    def name_get(self):
-        res = super(Partner,self).name_get()
-        data = []
-        for partner in self:
-            name = ''
-            if partner.ref:
-                name += '['
-                name += partner.ref
-                name += '] '
-            name += partner.name or ""
-            data.append((partner.id, name))
-        return data
-        
-    @api.model
-    def create(self,vals):
-        if vals.get('ref'):
-            if self.search([('ref','=',vals['ref'])]):
-                raise exceptions.Warning('Customer/Supplier code already exists')
-        res = super(Partner,self).create(vals)
-        return res
-    
-    ref = fields.Char(string='Internal Reference', required=True, index=True)   #make ref compulsory when creating a customer
     over_credit = fields.Boolean('Allow Over Credit?')
