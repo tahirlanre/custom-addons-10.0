a0701f85418d3367e3bdfd29ea33573239e35ed3
Tahir <tahirlanre@hotmail.com>
2018-12-08 06:28:15 +0100

    Initial commit

---
+++ stock_picking_report_custom/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Custom Stock Picking Report',
+    'description': """
+        Add received by column to Picking Operations and Delivery Slip Reports
+    """,
+    'author': 'Tahir Aduragba',
+    'depends': ['stock'],
+    'data': ['views/report_deliveryslip.xml'],
+    'installable': True,
+}
