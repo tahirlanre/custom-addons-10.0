aaca71d5f19be6c57c13cd0cca825804480cd407
Tahir <tahirlanre@hotmail.com>
2019-03-30 09:31:48 +0000

    Add dependency

--- account_asset_report/__manifest__.py
+++ account_asset_report/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','report_xlsx'],
+    'depends': ['base','report_xlsx', 'account_asset'],
 
     # always loaded
     'data': [
