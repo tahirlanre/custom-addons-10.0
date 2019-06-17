f3a8a7c2792aa05f8dd27b741da2bf7d2eb79c7f
Tahir <tahirlanre@hotmail.com>
2019-06-17 12:11:35 +0100

    Change field type

--- dmx_contact_directory/models/res_partner.py
+++ dmx_contact_directory/models/res_partner.py
@@ -20,7 +20,7 @@ class res_partner(models.Model):
     business_type = fields.Char(string='Business Type') #change to fields.Many2one
     phone2 = fields.Char(string='Phone 2')
     referral = fields.Char(string='Referral')
-    instagram_account = fields.Text(string='Instagram')
-    twitter_account = fields.Text(string='Twitter')
-    facebook_account = fields.Text(string='Facebook')
+    instagram_account = fields.Char(string='Instagram')
+    twitter_account = fields.Char(string='Twitter')
+    facebook_account = fields.Char(string='Facebook')
     
